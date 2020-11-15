820dab2e8d7b41ae310abea2ac58761aaa6174a3
Ziyodulla <zieu2000@gmail.com>
2020-11-15 20:58:25 +0500

    added burger menu

---
+++ scripts/nav.js
@@ -0,0 +1,21 @@
+const burgerBtn = document.querySelector('.burger');
+const mobileNav = document.querySelector('.mobile-nav');
+const overlay = document.querySelector('.overlay');
+
+const openBurgerMenu = () => {
+    console.log("burger opened");
+    mobileNav.classList.add('visible');
+    if (mobileNav.classList.contains('visible')) {
+        overlay.classList.add('visible');
+    } else {
+        overlay.classList.remove('visible');
+    }
+}
+
+const closeBurgerMenu = () => {
+    mobileNav.classList.remove('visible');
+    overlay.classList.remove('visible');
+}
+
+burgerBtn.addEventListener('click', openBurgerMenu);
+overlay.addEventListener('click', closeBurgerMenu);
