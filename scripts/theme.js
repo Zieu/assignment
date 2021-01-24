65eab38e75991ef0138ed4bb697d7e9eb8e7f846
Zieu <zieu2000@gmail.com>
2021-01-24 10:25:17 +0500

    added dark mode

---
+++ scripts/theme.js
@@ -0,0 +1,89 @@
+const galleryData = [
+  {
+      "title": "abcd",
+      "source": "https://images.unsplash.com/photo-1516716984596-1f6e47c5e986?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NDB8fHRodW1ibmFpbHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+      "thumb": "https://images.unsplash.com/photo-1516716984596-1f6e47c5e986?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NDB8fHRodW1ibmFpbHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=200&q=60",
+      "theme": "light",
+      "id": 0    
+  },
+  {
+      "title": "abcd",
+      "source": "https://images.unsplash.com/photo-1516716984596-1f6e47c5e986?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NDB8fHRodW1ibmFpbHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+      "thumb": "https://images.unsplash.com/photo-1516716984596-1f6e47c5e986?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NDB8fHRodW1ibmFpbHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=200&q=60",
+      "theme": "light",
+      "id": 1    
+  },
+  {
+      "title": "abcd",
+      "source": "https://images.unsplash.com/photo-1516716984596-1f6e47c5e986?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NDB8fHRodW1ibmFpbHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+      "thumb": "https://images.unsplash.com/photo-1516716984596-1f6e47c5e986?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NDB8fHRodW1ibmFpbHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=200&q=60",
+      "theme": "light",
+      "id": 2    
+  },
+  {
+      "title": "abcd",
+      "source": "https://images.unsplash.com/photo-1516716984596-1f6e47c5e986?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NDB8fHRodW1ibmFpbHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+      "thumb": "https://images.unsplash.com/photo-1516716984596-1f6e47c5e986?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NDB8fHRodW1ibmFpbHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=200&q=60",
+      "theme": "light",
+      "id": 3    
+  },
+  {
+      "title": "abcd",
+      "source": "https://images.unsplash.com/photo-1516716984596-1f6e47c5e986?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NDB8fHRodW1ibmFpbHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+      "thumb": "https://images.unsplash.com/photo-1516716984596-1f6e47c5e986?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NDB8fHRodW1ibmFpbHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=200&q=60",
+      "theme": "light",
+      "id": 4    
+  },
+  {
+      "title": "abcd",
+      "source": "https://images.unsplash.com/photo-1516716984596-1f6e47c5e986?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NDB8fHRodW1ibmFpbHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+      "thumb": "https://images.unsplash.com/photo-1516716984596-1f6e47c5e986?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NDB8fHRodW1ibmFpbHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=200&q=60",
+      "theme": "light",
+      "id": 5   
+  },
+  {
+      "title": "abcd",
+      "source": "https://images.unsplash.com/photo-1516716984596-1f6e47c5e986?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NDB8fHRodW1ibmFpbHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+      "thumb": "https://images.unsplash.com/photo-1516716984596-1f6e47c5e986?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NDB8fHRodW1ibmFpbHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=200&q=60",
+      "theme": "light",
+      "id": 6   
+  },
+  {
+      "title": "abcd",
+      "source": "https://images.unsplash.com/photo-1516716984596-1f6e47c5e986?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NDB8fHRodW1ibmFpbHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+      "thumb": "https://images.unsplash.com/photo-1516716984596-1f6e47c5e986?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NDB8fHRodW1ibmFpbHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=200&q=60",
+      "theme": "light",
+      "id": 7   
+  }
+]
+
+const cardContainer = document.querySelector(".gallery-cards");
+
+galleryData.map(image => {
+  cardContainer.innerHTML += `
+  <div class="gallery-card ${image.theme === "dark" ? "gallery-card--dark" : ""}">
+  <div class="gallery-card-content">
+    <img
+      class="gallery-card-img"
+      src="${image.thumb}"
+      alt="image #${image.title}"
+    />
+  </div>
+  <div class="gallery-card-line"></div>
+  <div class="gallery-card-title-wrapper">
+    <span class="gallery-card-text">${image.title}</span>
+    <button id="button-id-${image.id}" class="gallery-theme-toggler"></button>
+  </div>
+  </div>
+  `
+})
+
+galleryData.map(({id}) => {
+  const themeTogglerBtn = document.getElementById(`button-id-${id}`)
+  const card = document.querySelectorAll(".gallery-card");
+  themeTogglerBtn.addEventListener("click", toggleTheme)
+
+  function toggleTheme() {
+    card[id].classList.toggle("gallery-card--dark")
+  };
+})
